cc3313082ece017b69dd192bee8b16ceafb67e74
2774315 Jhon Alexander Urbano Pardo <jhonalexander2016.com@gmail.com>
2024-10-22 11:50:01 -0500

    falta terminar login y register

--- src/app/domains/pages/login-page/login-page.component.ts
+++ src/app/domains/pages/login-page/login-page.component.ts
@@ -1,19 +1,42 @@
 import { Component } from '@angular/core';
 import { AuthLayoutComponent } from "../../auth/auth-layout/auth-layout.component";
 import { Router, RouterOutlet } from '@angular/router';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [AuthLayoutComponent,RouterOutlet],
+  imports: [AuthLayoutComponent, RouterOutlet, NzFormModule, CommonModule, FormsModule,ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
-  go(){
+
+
+  go() {
     this.router.navigate(['/auth/register']);
+  }
+
+  formLogin = new FormGroup({
+    document: new FormControl('', [Validators.required]),
+    password: new  FormControl('', [Validators.required])
+  });
+
+
+  get fieldDocument(){
+    return this.formLogin.get('document') as FormControl;
+  }
+
+  get fielPassword(){
+    return this.formLogin.get('password') as FormControl;
+  }
+
+  login(){
+    return console.log(this.formLogin);
   }
 
 }
--- src/app/domains/pages/register-page/register-page.component.ts
+++ src/app/domains/pages/register-page/register-page.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { AuthLayoutComponent } from '@domains/auth/auth-layout/auth-layout.component';
+import { NzFormModule } from 'ng-zorro-antd/form';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [],
+  imports: [AuthLayoutComponent, RouterOutlet, NzFormModule, CommonModule, FormsModule,ReactiveFormsModule],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
+
+  formRegister = new FormGroup({
+    document: new  FormControl('', [Validators.required])
+  })
+
+
+  get fieldDocument(){
+    return this.formRegister.get('document') as FormControl;
+  }
 
 }
