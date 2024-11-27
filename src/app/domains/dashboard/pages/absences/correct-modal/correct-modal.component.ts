729fde0053006cded4879bfeb1fd237e15f8c54b
2774315 DANIELA DIAZ <calidad8844@gmail.com>
2024-11-27 11:40:35 -0500

    modal correct

---
+++ src/app/domains/dashboard/pages/absences/correct-modal/correct-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-correct-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './correct-modal.component.html',
+  styleUrl: './correct-modal.component.css'
+})
+export class CorrectModalComponent {
+
+}
