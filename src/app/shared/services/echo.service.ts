5032a80d0f3d8b9271661a8d34ee162c2dd4f741
bolaalcatras <jhonalexander2016.com@gmail.com>
2024-12-17 07:51:26 -0500

    bugs

--- src/app/shared/services/echo.service.ts
+++ src/app/shared/services/echo.service.ts
@@ -8,47 +8,47 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class WebSocketService {
-  private echo: Echo;
-  pusher = Pusher;
-  private tokenJWT = inject(TokenService)
-  private notificationSubject = new Subject<any>();
+  // private echo: Echo<'pusher'>;
+  // pusher = Pusher;
+  // private tokenJWT = inject(TokenService)
+  // private notificationSubject = new Subject<any>();
 
-  constructor() {
+  // constructor() {
 
-    const token = this.tokenJWT.getToken();
-    // console.log('Token en Echo:', token );
+  //   const token = this.tokenJWT.getToken();
+  //   // console.log('Token en Echo:', token );
 
-    this.echo = new Echo({
-      broadcaster: 'pusher',
-      key: 'odgldod1lgmvspxovpsi',
-      cluster: 'mt1',
-      wsHost: '127.0.0.1',
-      wsPort: 8080,
-      forceTLS: false,
-      disableStats: true,
-      encrypted: false, // Cambia esto si estás usando HTTPS
-      authEndpoint: 'http://127.0.0.1:8000/broadcasting/auth',
-      auth: {
-        headers: {
-          Authorization: `Bearer ${this.tokenJWT.getToken()}`,
-        },
-      },
-    });
+  //   this.echo = new Echo({
+  //     broadcaster: 'pusher',
+  //     key: 'odgldod1lgmvspxovpsi',
+  //     cluster: 'mt1',
+  //     wsHost: '127.0.0.1',
+  //     wsPort: 8080,
+  //     forceTLS: false,
+  //     disableStats: true,
+  //     encrypted: false, // Cambia esto si estás usando HTTPS
+  //     authEndpoint: 'http://127.0.0.1:8000/broadcasting/auth',
+  //     auth: {
+  //       headers: {
+  //         Authorization: `Bearer ${this.tokenJWT.getToken()}`,
+  //       },
+  //     },
+  //   });
 
-    // console.log(this.echo)
-  }
+  //   // console.log(this.echo)
+  // }
 
-  // Método para escuchar el canal privado
-  public listenToPrivateChannel(id: number) {
-    this.echo.channel('notifications')
-      .listen('SendMessage', (event: any) => {
-        console.log('Mensaje recibido:', event);
-        // Aquí puedes manejar el evento, por ejemplo, actualizar la interfaz
-      });
-    }
+  // // Método para escuchar el canal privado
+  // public listenToPrivateChannel(id: number) {
+  //   this.echo.channel('notifications')
+  //     .listen('SendMessage', (event: any) => {
+  //       console.log('Mensaje recibido:', event);
+  //       // Aquí puedes manejar el evento, por ejemplo, actualizar la interfaz
+  //     });
+  //   }
 
-    getNotifications() {
-      return this.notificationSubject.asObservable(); // Permite que otros componentes se suscriban
-    }
+  //   getNotifications() {
+  //     return this.notificationSubject.asObservable(); // Permite que otros componentes se suscriban
+  //   }
 
 }
